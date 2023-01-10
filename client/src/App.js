b53e576d9352ac0b177afad499774f32baeff255
joerdixon <joseph35dixon@gmail.com>
2023-01-09 21:10:25 -0800

    called components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 // Components
+import Header from './Components/Header/Header.jsx';
+import Navigation from './Components/Header/Navigation.jsx';
+import Artifacts from "./Components/Page/Artifacts/Artifacts.jsx";
+import Characters from "./Components/Page/Characters/Characters.jsx";
+import History from "./Components/Page/History/History.jsx";
+import Home from "./Components/Page/Home/Home.jsx";
+import Map from "./Components/Page/Map/Map.jsx";
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -13,6 +20,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
+        <Navigation />
+        <Artifacts />
+        <Characters />
+        <History />
+        <Home />
+        <Map />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
