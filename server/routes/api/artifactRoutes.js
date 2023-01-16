8aefd02a1a352f44280fb81232f8727b34e6297b
joerdixon <joseph35dixon@gmail.com>
2023-01-16 13:12:42 -0800

    more routing fs

---
+++ server/routes/api/artifactRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const {
+    getArtifacts
+} = require('../../controllers/artifactController')
+
+router.get('/hi', (req, res) => {
+    try {
+        res.status(200).json({message: 'hi joe'})
+    } catch (err) {
+        res.status(500).json({err:err})
+    }
+})
+
+router.route('/').get(getArtifacts)
+
+module.exports = router;
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const characterRoutes = require('./characterRoutes');
+const artifactRoutes = require('./artifactRoutes');
+const locationRoutes = require('./locationRoutes');
 
-router.use("/char", characterRoutes)
+router.use("/character", characterRoutes)
+router.use("/artifact", artifactRoutes)
+router.use("/location", locationRoutes)
 
 module.exports = router;
