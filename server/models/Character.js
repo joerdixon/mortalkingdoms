c721530753f782378b260bb8daa21dd8f2481ed6
joerdixon <joseph35dixon@gmail.com>
2023-01-15 20:54:31 -0800

    connection + character shape

--- server/models/Character.js
+++ server/models/Character.js
@@ -4,20 +4,39 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Character extends Model {}
+class Character extends Model { }
 
 Character.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        epitaph: {
+            type: DataTypes.STRING,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+        },
+        height: {
+            type: DataTypes.INTEGER,
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+        },
+        affiliation: {
+            type: DataTypes.STRING,
+        }
+
+    },
+    {
+        sequelize,
     }
-  },
-  {
-    sequelize,
-  }
 );
 
 module.exports = Character;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const Character = require('./Character.js');
 const Artifact = require('./Artifact.js');
 const History = require('./History.js');
+
+module.exports = { Character, Artifact, History }
