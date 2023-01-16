add5cae7f6782d9d3d71cf4987621680b1563e73
joerdixon <joseph35dixon@gmail.com>
2023-01-15 20:10:35 -0800

    nav skeleton in

--- client/src/Components/Header/Navigation.jsx
+++ client/src/Components/Header/Navigation.jsx
@@ -3,14 +3,14 @@ import mklogo from '../../assets/icons/mklogo.png'
 
 function Navigation() {
 return(
-    <nav className='flex'>
-        <a href="">Link</a>
-        <a href="">Link</a>
-        <a href=""><img src={mklogo} alt="Mortal Kingdoms Logo" className='w-24' /></a>
-        <a href="">Link</a>
+    <nav className='flex justify-evenly items-center w-screen bg-gray-200 h-[50px]'>
+        <a href="">Artifacts</a>
+        <a href="">People</a>
+        <a href=""><img src={mklogo} alt="Mortal Kingdoms Logo" className='w-20 mt-6' /></a>
+        <a href="">Map</a>
         <a href="">Link</a>
     </nav>
 )
 };
 
-export default Navigation;
+export default Navigation;  
