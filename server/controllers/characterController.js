d3f68247898270e04b8bda61109500f268690953
joerdixon <joseph35dixon@gmail.com>
2023-01-19 15:32:28 -0800

    more comment cleanup

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -3,8 +3,10 @@ const { Character, Artifact } = require('../models');
 // Get all characters 
 async function getCharacters(req, res) {
     try {
+        // Query the database for all characters
         const characterData = await Character.findAll({
             include: {
+                // Include the artifacts held by characters
                 model: Artifact
             }
         });
@@ -18,10 +20,13 @@ async function getCharacters(req, res) {
 // Get one character
 async function getCharacter(req, res) {
     try {
+        // Query the database for character with a specific name
         const characterData = await Character.findOne({
             where: { name: req.params.name },
+            // Include any artifacts that characters holds.
             include: { model: Artifact }
         })
+        // If the character is not found, notify the user.
         if (characterData === null) {
             console.log("Not found")
             res.status(200).json({ msg: "Character not found" })
@@ -34,6 +39,7 @@ async function getCharacter(req, res) {
     }
 }
 
+// Export these functions for use in routes.
 module.exports = {
     getCharacters,
     getCharacter
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,4 +4,5 @@ DROP DATABASE IF EXISTS mortalkingdoms_db;
 -- CREATE DATABASE
 CREATE DATABASE mortalkingdoms_db;
 
+-- USE DATABASE
 USE mortalkingdoms_db;
