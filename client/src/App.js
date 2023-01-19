61649bc8d171a054d0e38ae1628993672e9b409c
joerdixon <joseph35dixon@gmail.com>
2023-01-18 16:50:32 -0800

    database relations started

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom"
 // Components
 // TODO: is there any real use for the header?
-import Header from './Components/Header/Header.jsx';
+// import Header from './Components/Header/Header.jsx';
 import Navigation from './Components/Header/Navigation.jsx';
 import Artifacts from "./Components/Page/Artifacts/Artifacts.jsx";
 import Characters from "./Components/Page/Characters/Characters.jsx";
@@ -15,14 +15,6 @@ import Home from "./Components/Page/Home/Home.jsx";
 import Map from "./Components/Page/Map/Map.jsx";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api/characters/")
-      .then((res) => res.json())
-      .then((data) => setData(data[0].name));
-  }, []);
-
   return (
     <div className="App">
       {/* The Router object contains all routes and links */}
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,15 @@
+// Model imports
 const Character = require('./Character.js');
 const Artifact = require('./Artifact.js');
-const Faction = require('./Faction.js')
+const Faction = require('./Faction.js');
 
+// Factions can have multiple characters, characters belong to thier most relevant faction.
+Character.belongsTo(Faction);
+Faction.hasMany(Character);
+
+// Artifacts can belong to a character
+Artifact.belongsTo(Character);
+Character.hasMany(Artifact);
+
+// Export the models.
 module.exports = { Character, Artifact, Faction }
