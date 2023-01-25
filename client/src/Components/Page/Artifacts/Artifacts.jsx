dac47b2270c7991191af8816f445ddf663bb082b
joerdixon <joseph35dixon@gmail.com>
2023-01-24 20:03:06 -0800

    artifact and character component desc

--- client/src/Components/Page/Artifacts/Artifacts.jsx
+++ client/src/Components/Page/Artifacts/Artifacts.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import api from '../../../utils/api';
 
+// The Artifacts page will house all artifacts found in the database.
+
+// Users will be able to view and sort artifacts based on their age (And potentially more things.)
+
+// When a user clicks on an artifact, they will be shown an enlarged view and all information about that artifact.
+
+// This includes the character that owns the artifact (if there is one.)
+
 function Artifacts() {
     const [artifacts, setArtifacts] = React.useState([])
 
     React.useEffect(() => {
+        // TODO: This is running twice everytime.
         async function fetchArtifacts() {
             const data = await api.getAllArtifacts();
             for (let i = 0; i < data.length; i++) {
--- client/src/Components/Page/Characters/Characters.jsx
+++ client/src/Components/Page/Characters/Characters.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import api from '../../../utils/api';
 
+// The Characters page will contain a list of all characters stored in the database.
+
+// Users will be able to view and sort characters based on a variety of properties.
+
+// When a user clicks on a character, they will be shown an enlarged view and all information about that character.
+
+// This includes any artifacts owned by that character and the faction that character belongs to.
+
 function Characters() {
     // All characters are stored here. (Not quite)
     const [characters, setCharacters] = React.useState(null);
