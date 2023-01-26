f1ec5082b947a4a0e70fd9fb9ea61c98c6c1c00b
joerdixon <joseph35dixon@gmail.com>
2023-01-26 15:42:48 -0800

    navbar basic hover

--- client/src/Components/Header/Navigation.jsx
+++ client/src/Components/Header/Navigation.jsx
@@ -6,11 +6,11 @@ import mklogo from '../../assets/icons/mklogo.png'
 function Navigation() {
     return (
         <nav className='flex justify-evenly items-center w-screen bg-[#111] h-[50px]'>
-            <Link className='w-20 text-center' to='/artifacts'>Artifacts</Link>
-            <Link className='w-20 text-center' to='/characters'>People</Link>
-            <Link className='w-20 text-center' to='/'><img src={mklogo} alt="Mortal Kingdoms Logo" className='w-20 mt-6' /></Link>
-            <Link className='w-20 text-center' to='/map'>Map</Link>
-            <Link className='w-20 text-center' to='/factions'>Factions</Link>
+            <Link className='w-20 text-center hover:translate-y-1 transition-all' to='/artifacts'>Artifacts</Link>
+            <Link className='w-20 text-center hover:translate-y-1 transition-all' to='/characters'>People</Link>
+            <Link className='w-20 text-center hover:translate-y-1 transition-all' to='/'><img src={mklogo} alt="Mortal Kingdoms Logo" className='w-20 mt-6' /></Link>
+            <Link className='w-20 text-center hover:translate-y-1 transition-all' to='/map'>Map</Link>
+            <Link className='w-20 text-center hover:translate-y-1 transition-all' to='/factions'>Factions</Link>
         </nav>
     )
 };
