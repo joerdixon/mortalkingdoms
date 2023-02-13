77c873be049909a59dac09206b14b19f7493a8c3
joerdixon <joseph35dixon@gmail.com>
2023-02-12 19:19:13 -0800

    todo update

--- client/src/Components/Header/Navigation.jsx
+++ client/src/Components/Header/Navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import mklogo from '../../assets/icons/mklogo.png'
 
+// TODO: Update hover effect
 
 function Navigation() {
     return (
